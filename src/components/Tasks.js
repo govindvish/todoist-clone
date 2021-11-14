5a4c5fc61507c3099e2a40c7d970e48aa8808e11
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2021-11-14 19:03:49 +0530

    Added code to display tasks based on project.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,29 @@
+import { useEffect } from 'react';
+import { collatedTasks } from '../constants';
+import { useProjectsValue, useSelectedProjectValue } from '../context';
+import { collatedTasksExist, getCollatedTitle, getTitle } from '../helpers';
 import { useTasks } from '../hooks';
 import { Checkbox } from './Checkbox';
 
 export const Tasks = () => {
-  const { tasks } = useTasks('1');
-  console.log(tasks);
-  const projectName = '';
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
+
+  let projectName = '';
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  });
+
   return (
     <div className='tasks' data-testid='tasks'>
       <h2 data-testid='project-name'>{projectName}</h2>
