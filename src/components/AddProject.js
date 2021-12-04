9fa397fb69f2cac5f56231180296cf313ad9531b
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2021-12-04 21:25:04 +0530

    Fix the issue of app breaking on add project.

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -8,7 +8,7 @@ export const AddProject = ({ shouldShow = false }) => {
   const [projectName, setProjectName] = useState('');
 
   const projectId = generatePushId();
-  const { setProjects } = useProjectsValue();
+  const { projects, setProjects } = useProjectsValue();
 
   const addProject = () =>
     projectName &&
@@ -17,7 +17,7 @@ export const AddProject = ({ shouldShow = false }) => {
       .collection('projects')
       .add({ projectId, name: projectName, userId: 'Pq9NErhvn0yvwzG8ybzZ' })
       .then(() => {
-        setProjects([]);
+        setProjects([...projects]);
         setProjectName('');
         setShow(false);
       });
