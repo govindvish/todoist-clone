1212b6a5804edfa21c0eeb11101dfd7d56cd3468
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2021-12-04 21:11:27 +0530

    Added quick add task.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
+import { AddTask } from '../AddTask';
 
 export const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
   return (
     <header className='header' data-testid='header'>
       <nav>
@@ -9,7 +14,14 @@ export const Header = ({ darkMode, setDarkMode }) => {
         </div>
         <div className='settings'>
           <ul>
-            <li data-testid='quick-add-task-action' className='settings__add'>
+            <li
+              data-testid='quick-add-task-action'
+              className='settings__add'
+              onClick={() => {
+                setShowQuickAddTask(true);
+                setShouldShowMain(true);
+              }}
+            >
               +
             </li>
             <li
@@ -22,6 +34,13 @@ export const Header = ({ darkMode, setDarkMode }) => {
           </ul>
         </div>
       </nav>
+
+      <AddTask
+        showAddTaskMain={false}
+        shouldShowMain={shouldShowMain}
+        showQuickAddTask={showQuickAddTask}
+        setShowQuickAddTask={setShowQuickAddTask}
+      />
     </header>
   );
 };
